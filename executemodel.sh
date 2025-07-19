6740571221aa9f15fe4529a6b384a87d97f3c9fd
gabi107 <gabitheis107@gmail.com>
2025-07-18 21:37:44 -0500

    execute model

--- executemodel.sh
+++ executemodel.sh
@@ -1,6 +1,6 @@
 #GCN-GAT model training script
-python ./src/GNN_workflow.py --gpu 0 --train --randSplit --path './models/GAT_model/GAT_early_stop_skipCV_data2_best_opt2' --data './data/surfactant_data2/surfactant_data.csv' --gnn_model GAT   --seed 104 --test_size 0.10 --epochs 1500 --early_stop --patience 40 --skip_cv --batch_size 5 --unit_per_layer 384 --lr 0.0021
-python ./src/GNN_workflow.py --gpu 0 --train --randSplit --path './models/GCN_model/GCN_early_stop_skipCV_data2_best_opt2' --data './data/surfactant_data2/surfactant_data.csv' --gnn_model GCNReg   --seed 2024 --test_size 0.10 --epochs 1000 --early_stop --patience 40 --skip_cv --batch_size 32 --unit_per_layer 128 --lr 0.00135
+python ./src/GCN-GAT/GNN_workflow.py --gpu 0 --train --randSplit --path './models/GAT_model/GAT_early_stop_skipCV_data2_best_opt2' --data './data/surfactant_data2/surfactant_data.csv' --gnn_model GAT   --seed 104 --test_size 0.10 --epochs 1500 --early_stop --patience 40 --skip_cv --batch_size 5 --unit_per_layer 384 --lr 0.0021
+python ./src/GCN-GAT/GNN_workflow.py --gpu 0 --train --randSplit --path './models/GCN_model/GCN_early_stop_skipCV_data2_best_opt2' --data './data/surfactant_data2/surfactant_data.csv' --gnn_model GCNReg   --seed 2024 --test_size 0.10 --epochs 1000 --early_stop --patience 40 --skip_cv --batch_size 32 --unit_per_layer 128 --lr 0.00135
 
 #PharmaHGT model training script
 
